f5c86cad50fe42b230bde287ea873d3167747f47
abdelmoughiterrabih <moughit3000@gmail.com>
2024-07-15 17:03:48 +0200

    test

---
+++ src/main/java/com/diagnostic_immobilier_backend/demo/DiagnosticImmobilierBackendApplication.java
@@ -0,0 +1,13 @@
+package com.diagnostic_immobilier_backend.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DiagnosticImmobilierBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DiagnosticImmobilierBackendApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/diagnostic_immobilier_backend/demo/DiagnosticImmobilierBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.diagnostic_immobilier_backend.demo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DiagnosticImmobilierBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
